f705e60bd93f6f501a0f854a0693dec27b085b22
Hai Guo <deliberatekids@gmail.com>
2023-01-09 15:56:41 +0800

    add one test case/check correction after handled by LyricsParser

--- app/src/androidTest/java/io/agora/examples/lyrics_view/LyricsInstrumentedTest.java
+++ app/src/androidTest/java/io/agora/examples/lyrics_view/LyricsInstrumentedTest.java
@@ -16,9 +16,10 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 
 import io.agora.examples.utils.ResourceHelper;
-import io.agora.lyrics_view.LrcLoadUtils;
-import io.agora.lyrics_view.bean.LrcData;
-import io.agora.lyrics_view.bean.LrcEntryData;
+
+import io.agora.lyrics_view.v11.model.LyricsLineModel;
+import io.agora.lyrics_view.v11.model.LyricsModel;
+import io.agora.lyrics_view.v11.utils.LyricsParser;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -41,27 +42,29 @@ public class LyricsInstrumentedTest {
     public void parseOneAndOnlyOneLineXmlFile() {
         // specified to 810507.xml
 
+        String fileNameOfSong = "810507.xml";
+        String songArtist = "张学友";
+
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        String oneAndOnlyOneLineXmlFileContent = ResourceHelper.loadAsString(appContext, "810507.xml");
-        assertEquals(true, oneAndOnlyOneLineXmlFileContent.contains("张学友"));
+        String oneAndOnlyOneLineXmlFileContent = ResourceHelper.loadAsString(appContext, fileNameOfSong);
+        assertEquals(true, oneAndOnlyOneLineXmlFileContent.contains(songArtist));
 
-        File target = ResourceHelper.copyAssetsToCreateNewFile(appContext, "810507.xml");
-        LrcData parsedLrc = LrcLoadUtils.parse(target);
+        File target = ResourceHelper.copyAssetsToCreateNewFile(appContext, fileNameOfSong);
+        LyricsModel parsedLyrics = LyricsParser.parse(target);
 
-        System.out.println(TAG + ": " + "Lines count for this lyrics " + parsedLrc.entrys.size());
-        Log.d(TAG, "Lines count for this lyrics " + parsedLrc.entrys.size());
+        Log.d(TAG, "Line count for this lyrics " + parsedLyrics.lines.size());
 
-        for (LrcEntryData item : parsedLrc.entrys) {
-            System.out.println(TAG + ": " + "Line summary: " + item.getStartTime() + " ~ " + item.getEndTime() + " " + item.tones.size());
+        for (LyricsLineModel line : parsedLyrics.lines) {
+            Log.d(TAG, "Line summary: " + line.getStartTime() + " ~ " + line.getEndTime() + " " + line.tones.size());
         }
 
         // 810507.xml has 42 lines
-        assertTrue(parsedLrc.entrys.size() == 42);
+        assertTrue(parsedLyrics.lines.size() == 42);
 
         // The 7th line contains '泪' '慢' '慢' '流' '慢' '慢' '收'
-        LrcEntryData the7thLine = parsedLrc.entrys.get(6);
-        long startOf7thLine = parsedLrc.entrys.get(6).getStartTime();
-        long endOf7thLine = parsedLrc.entrys.get(6).getEndTime();
+        LyricsLineModel the7thLine = parsedLyrics.lines.get(6);
+        long startOf7thLine = parsedLyrics.lines.get(6).getStartTime();
+        long endOf7thLine = parsedLyrics.lines.get(6).getEndTime();
         assertTrue(endOf7thLine - startOf7thLine > 0);
         assertTrue(TextUtils.equals("泪", the7thLine.tones.get(0).word) && the7thLine.tones.get(0).pitch == 176);
         assertTrue(TextUtils.equals("慢", the7thLine.tones.get(1).word) && the7thLine.tones.get(1).pitch == 0);
@@ -72,9 +75,9 @@ public class LyricsInstrumentedTest {
         assertTrue(TextUtils.equals("收", the7thLine.tones.get(6).word) && the7thLine.tones.get(6).pitch == 150);
 
         // The 41th line contains '你' '何' '忍' '远' '走' '高' '飞'
-        LrcEntryData the41thLine = parsedLrc.entrys.get(40);
-        long startOf41thLine = parsedLrc.entrys.get(40).getStartTime();
-        long endOf41thLine = parsedLrc.entrys.get(40).getEndTime();
+        LyricsLineModel the41thLine = parsedLyrics.lines.get(40);
+        long startOf41thLine = parsedLyrics.lines.get(40).getStartTime();
+        long endOf41thLine = parsedLyrics.lines.get(40).getEndTime();
         assertTrue(endOf41thLine - startOf41thLine > 0);
         assertTrue(TextUtils.equals("你", the41thLine.tones.get(0).word) && the41thLine.tones.get(0).pitch == 0);
         assertTrue(TextUtils.equals("何", the41thLine.tones.get(1).word) && the41thLine.tones.get(1).pitch == 0);
@@ -83,5 +86,29 @@ public class LyricsInstrumentedTest {
         assertTrue(TextUtils.equals("走", the41thLine.tones.get(4).word) && the41thLine.tones.get(4).pitch == 0);
         assertTrue(TextUtils.equals("高", the41thLine.tones.get(5).word) && the41thLine.tones.get(5).pitch == 0);
         assertTrue(TextUtils.equals("飞", the41thLine.tones.get(6).word) && the41thLine.tones.get(6).pitch == 0);
+    }
+
+    @Test
+    public void parseSameTimestampForStartAndPreviousEndXmlFile() {
+        // specified to 825003.xml
+
+        String fileNameOfSong = "825003.xml";
+        String songTitle = "净化空间";
+
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String sameTimestampForStartOfCurrentLineAndEndOfPreviousLineXmlFileContent = ResourceHelper.loadAsString(appContext, fileNameOfSong);
+        assertEquals(true, sameTimestampForStartOfCurrentLineAndEndOfPreviousLineXmlFileContent.contains(songTitle));
+
+        File target = ResourceHelper.copyAssetsToCreateNewFile(appContext, fileNameOfSong);
+        LyricsModel parsedLyrics = LyricsParser.parse(target);
+
+        Log.d(TAG, "Line count for this lyrics(" + songTitle + ") " + parsedLyrics.lines.size());
+
+        for (LyricsLineModel line : parsedLyrics.lines) {
+            Log.d(TAG, "Line summary: " + line.getStartTime() + " ~ " + line.getEndTime() + " " + line.tones.size());
+        }
+
+        // 825003.xml has 30 lines
+        assertTrue(parsedLyrics.lines.size() == 30);
     }
 }
