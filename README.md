2882ca19d73c78c29382250359b52fba93a81017
Hai Guo <deliberatekids@gmail.com>
2023-03-07 16:03:32 +0800

    update handle pitch algo
    and update version to 1.0.31

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ allprojects {
 ```
 dependencies {
     ...
-    implementation 'com.github.AgoraIO-Community:LyricsView:1.0.28'
+    implementation 'com.github.AgoraIO-Community:LyricsView:1.0.31'
 }
 ```
 
--- lrcview/src/main/java/io/agora/lyrics_view/VoicePitchChanger.java
+++ lrcview/src/main/java/io/agora/lyrics_view/VoicePitchChanger.java
@@ -12,22 +12,14 @@ public class VoicePitchChanger {
     ///   - wordMaxPitch: 最大值 来自标准值
     /// - Returns: 处理后的值
     public double handlePitch(double wordPitch,
-                       double voicePitch,
-                       double wordMaxPitch) {
+                              double voicePitch,
+                              double wordMaxPitch) {
         if (voicePitch <= 0) {
             return 0;
         }
 
         n += 1;
         double gap = wordPitch - voicePitch;
-
-        // 这个算法问题
-        // 1) 第一个 pitch 的时候直接返回 refPitch，但这在默认整首歌当中都只有一次。
-        // 是否需要每句歌词都应用这样的逻辑(也就是累积效应只在每一句当中)。
-        // 2) 看看是否要增加 abs(pitch - refPitch) / maxPitch <= 0.2f 的时候，可以直接返回 pitch
-        if (Math.abs(gap) < 1) { // The chance would be not much, try to apply `gap / wordMaxPitch <= 0.2f` if necessary
-            return Math.min(voicePitch, wordMaxPitch);
-        }
 
         offset = offset * (n - 1) / n + gap / n;
 
@@ -37,7 +29,28 @@ public class VoicePitchChanger {
             offset = Math.min(offset, wordMaxPitch * 0.4);
         }
 
-        return Math.min(voicePitch + offset, wordMaxPitch);
+        // 这个算法问题
+        // 1) 第一个 pitch 的时候直接返回 refPitch，但这在默认整首歌当中都只有一次。
+        // 是否需要每句歌词都应用这样的逻辑(也就是累积效应只在每一句当中)。
+        // 2) 看看是否要增加 abs(pitch - refPitch) / maxPitch <= 0.2f 的时候，可以直接返回 pitch
+        if (Math.abs(gap) < 1) { // The chance would be not much, try to apply `gap / wordMaxPitch <= 0.2f` if necessary
+            return Math.min(voicePitch, wordMaxPitch);
+        }
+
+        switch (n) {
+            case 1:
+                return Math.min(voicePitch + 0 * offset, wordMaxPitch);
+            case 2:
+                return Math.min(voicePitch + 0.2 * offset, wordMaxPitch);
+            case 3:
+                return Math.min(voicePitch + 0.4 * offset, wordMaxPitch);
+            case 4:
+                return Math.min(voicePitch + 0.6 * offset, wordMaxPitch);
+            case 5:
+                return Math.min(voicePitch + 0.8 * offset, wordMaxPitch);
+            default:
+                return Math.min(voicePitch + offset, wordMaxPitch);
+        }
     }
 
     public void reset() {
